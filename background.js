c881fdbb1a82f9d5d459e3543a2a1cc5d46ba58f
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2026-04-15 22:36:01 +0530

    Add message listener for incident storage management

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
-
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.type === "storeIncident") {
+    chrome.storage.local.get({clearedIncidents: []}, (result) => {
+      const updated = result.clearedIncidents.concat([msg.data]);
+      chrome.storage.local.set({clearedIncidents: updated});
+    });
+  }
+  if (msg.type === "clearIncidents") {
+    chrome.storage.local.set({clearedIncidents: []});
+  }
+});
